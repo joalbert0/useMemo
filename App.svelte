de6f6827c8c88fbe5f9ad00d659b0564edf1c149
joalbert0 <48811361+joalbert0@users.noreply.github.com>
2022-05-13 19:00:02 -0500

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,42 @@
+<script>
+ import Button from "./Button.svelte";
+ let val1 = 0;
+ let val2 = 0;
+ let val3 = 0;
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+  }
+  .row {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    margin: 10px;
+  }
+  .block {
+    width: 120px;
+  }
+</style>
+
+<main>
+<div class="row" >
+<div class="block"><p>val1: {val1}</p> </div>
+<div class="block"><Button /></div>
+</div>
+
+<div class="row" >
+<div class="block"><p>val2: {val2}</p> </div>
+<div class="block"><Button /></div>
+</div>
+
+<div class="row" >
+<div class="block"><p>val3: {val3}</p> </div>
+<div class="block"><Button /></div>
+</div>
+
+	
+</main>
---
+++ Button.svelte
@@ -0,0 +1,23 @@
+<script>
+	let count = 0;
+
+	function handleClick() {
+	  count += 1;
+	}
+
+	console.log("hello");
+</script>
+
+<style>
+	button {
+	  background: #ff3e00;
+	  color: white;
+	  border: none;
+	  padding: 10px 12px;
+	  border-radius: 3px;
+	}
+</style>
+
+<button on:click={handleClick}>
+  Click
+</button>
